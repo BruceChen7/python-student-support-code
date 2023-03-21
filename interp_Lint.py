4aba2b61866597a2e6fb0fd63babe83ab37152e1
BruceChen7 <bofengqiye7@gmail.com>
2023-03-21 15:41:47 +0800

    feat: add more notes

--- interp_Lint.py
+++ interp_Lint.py
@@ -1,19 +1,25 @@
 from ast import *
+# 导入输入函数
 from utils import input_int, add64, sub64, neg64
 
 def interp_exp(e):
+    # 是python3.10 的高级特性
     match e:
         case BinOp(left, Add(), right):
             l = interp_exp(left); r = interp_exp(right)
             return add64(l, r)
+        # 减法
         case BinOp(left, Sub(), right):
             l = interp_exp(left); r = interp_exp(right)
             return sub64(l, r)
         case UnaryOp(USub(), v):
             return neg64(interp_exp(v))
+        # 常量值
         case Constant(value):
             return value
+        # 遇到模式
         case Call(Name('input_int'), []):
+            # 返回输出
             return input_int()
         case _:
             raise Exception('error in interp_exp, unexpected ' + repr(e))
@@ -21,8 +27,10 @@ def interp_exp(e):
 def interp_stmt(s):
     match s:
         case Expr(Call(Name('print'), [arg])):
+            #  输出结果
             print(interp_exp(arg))
         case Expr(value):
+            # 解释语言
             interp_exp(value)
         case _:
             raise Exception('error in interp_stmt, unexpected ' + repr(s))
@@ -35,7 +43,7 @@ def interp(p):
         case _:
             raise Exception('error in interp, unexpected ' + repr(p))
 
-# This version is for InterpLvar to inherit from 
+# This version is for InterpLvar to inherit from
 class InterpLint:
   def interp_exp(self, e, env):
     match e:
@@ -60,6 +68,7 @@ class InterpLint:
   # it enables the handling of Goto in interp_Cif.py.
   def interp_stmt(self, s, env, cont):
     match s:
+      # 遇到print
       case Expr(Call(Name('print'), [arg])):
         val = self.interp_exp(arg, env)
         print(val, end='')
@@ -69,7 +78,7 @@ class InterpLint:
         return self.interp_stmts(cont, env)
       case _:
         raise Exception('error in interp_stmt, unexpected ' + repr(s))
-    
+
   def interp_stmts(self, ss, env):
     match ss:
       case []:
@@ -83,10 +92,12 @@ class InterpLint:
         self.interp_stmts(body, {})
       case _:
         raise Exception('error in interp, unexpected ' + repr(p))
-    
+
 if __name__ == "__main__":
+  # 定义终结符，常量8
   eight = Constant(8)
   neg_eight = UnaryOp(USub(), eight)
+  # 定义输入函数
   read = Call(Name('input_int'), [])
   ast1_1 = BinOp(read, Add(), neg_eight)
   pr = Expr(Call(Name('print'), [ast1_1]))
